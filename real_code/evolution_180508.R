8d673e72ceb47f1a98655c7bf25fa69395ad022a
Yangsuhyung <sara2601@naver.com>
2018-05-31 18:54:25 +0900

    change combn to expand.grid
    combn is not caculated fast. so changed into expend.grid

--- real_code/evolution_180508.R
+++ real_code/evolution_180508.R
@@ -1,14 +1,17 @@
 source("round_180507.R")
-evolution <- function(A=8, D=8, Cc=8, Av=8, round=5, out=5, iter = 20, error = 0){
+evolution <- function(A=10, D=10, Cc=10, Av=10, round=5, out=5, iter = 20, error = 0, rep=500){
   match <- rep(c('A', 'D', 'Cc', 'Av'), c(A, D, Cc, Av))
-  league <- combn(1:length(match), 2); Vis <- list(); s <- list()
+  x <- y<- 1: length(match)
+  d1 <- expand.grid(x=x, y=y); league <- d1[d1[,1]<d1[,2],]
+  k <- 1; Vis <- list(); s <- list()
+  
   for(j in 1:iter){
     b <- rep(0, length(match)); names(b) <- match
     for(i in 1:ncol(league)){
       first <- match[league[1,i]]; second <- match[league[2,i]]
       result.1 <- game(round, first, second, error)
-      b[league[,i][1]] <- b[league[,i][1]]+result.1[[2]][1]
-      b[league[,i][2]] <- b[league[,i][2]]+result.1[[2]][2]
+      b[league[1,i]] <- b[league[1,i]]+result.1[[2]][1]
+      b[league[2,i]] <- b[league[2,i]]+result.1[[2]][2]
     }
     s[[j]] <- b
     if(length(unique(s[[j]]))==1) break
