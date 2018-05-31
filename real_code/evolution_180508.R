f77fcce0e4b02e24ed27d4c6a96e6623edb1e928
Yangsuhyung <sara2601@naver.com>
2018-05-31 20:42:36 +0900

    break condition is uploaed
    1. break condition is added
    2. visualization is organized

--- real_code/evolution_180508.R
+++ real_code/evolution_180508.R
@@ -3,7 +3,6 @@ evolution <- function(A=10, D=10, Cc=10, Av=10, round=5, out=5, iter = 20, error
   x <- y <- 1:length(match)
   d1 <- expand.grid(x=x, y=y); league <- d1[d1[,1]<d1[,2],]
   k <- 1; Vis <- list(); s <- list()
-  
   for(j in 1:iter){
     b <- rep(0, length(match)); names(b) <- match
     for(i in 1:nrow(league)){
@@ -13,17 +12,18 @@ evolution <- function(A=10, D=10, Cc=10, Av=10, round=5, out=5, iter = 20, error
       b[league$y[i]] <- b[league$y[i]]+result.1[[2]][2]
     }
     s[[j]] <- b
-    if(length(unique(s[[j]]))==1) break
+    if(length(unique(s[[j]]))==1 | length(unique(match))==1) break
     c <- sample(b); d <- sort(c)
     match <- c(rev(d)[1:out], d[-c(1:out)])
     match <- names(match)
     Vis[[j]] <- sort(match)
   }
   return(Vis)
-# return(s)
+  #  return(s)
 }
-ev.plot <- function(A=8, D=8, Cc=8, Av=8, round=5, out=5, iter = 20, error = 0){
+ev.plot <- function(A=8, D=15, Cc=8, Av=8, round=5, out=5, iter = 30, error = 0){
   k <- evolution(A, D, Cc, Av, round, out, iter, error)
+  #k <- evolution(A=8, D=15, Cc=8, Av=8, round=5, out=5, iter = 30, error = 0)
   tab <- matrix(0, length(k), 4)
   colnames(tab) <- c("A", "D", "Cc", "Av")
   for (i in 1:length(k)){
@@ -33,8 +33,11 @@ ev.plot <- function(A=8, D=8, Cc=8, Av=8, round=5, out=5, iter = 20, error = 0){
     tab[i,4] <- length(which(k[[i]]=="Av"))
   }
   rownames(tab) <- paste("trial", 1:length(k))
-  par(las=2)
-  print(barplot(t(apply(tab, 2, rev)), horiz=TRUE, space=rep(0, length(k)), main="Who will win in the long run?", xlab="# of each character", ylab="trial", col=c("red", "yellow", "green", "blue")))
-  print(legend("topright", legend=c("Angel", "Devil", "Copycat", "Avengers"), fill=c("red", "yellow", "green", "blue"), bg="gray"))
+  par(las=2); par(mar=c(5.1, 4.1, 4.1, 8.1), xpd=T)
+  print(barplot(t(apply(tab, 2, rev)), horiz=TRUE, space=rep(0, length(k)), main="Who will win in the long run?", xlim = c(0, length(k[[1]])),
+                xlab="# of each character", ylab="", col=c("tomato", "gold", "mediumaquamarine", "slateblue2")))
+  print(legend("topright", inset = c(-0.35, 0.1), box.lty = 0, bty != "n", title = "Strategy",
+               legend=c("Angel", "Devil", "Copycat", "Avengers"), fill=c("tomato", "gold", "mediumaquamarine", "slateblue2"), bg=c("gray87")))
+  print(legend("top", inset = c(0, -0.12), legend=paste("mistake probability = ", error), bty ="n", pch=NA))
 }
-ev.plot()
+ev.plot(A=10, D=10, Cc=10, Av=10, round=5, out=5, iter = 20, error = 0.05)
