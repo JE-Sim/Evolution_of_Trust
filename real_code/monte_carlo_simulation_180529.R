39d3c929b7d81f785a1225e482b8a92881c633a8
JE-Sim <33321949+JE-Sim@users.noreply.github.com>
2018-06-23 14:02:05 +0900

    add stat.simul()
    simulation based on stationary distribution.
    fix simul function

--- real_code/monte_carlo_simulation_180529.R
+++ real_code/monte_carlo_simulation_180529.R
@@ -1,13 +1,13 @@
 #simulation
 simul <- function(type, nA, nD, nCc, nAv, nround, nsim = 1e+5, error=0){
   mat <- Pm(type, nA, nD, nCc, nAv, error)
-  M <- nrow(mat); score1 <- score[1,floor(as.numeric(rownames(mat)[-1]))]
+  M <- ncol(mat); score1 <- score[1,floor(as.numeric(colnames(mat)[-1]))]
   point <- unlist(c(0, score1)); 
   x <- rep(0,nsim);
   for (j in 1:nsim) {
     temp <- sample(M, 1, prob=mat[1,])
     x[j] <- floor(point[temp])
-    for (i in 1:nround){  
+    for (i in 2:nround){  
       temp <- sample(M, 1, prob=mat[temp,]) 
       x[j] <- x[j] + floor(point[temp])
     }
@@ -18,3 +18,21 @@ simul <- function(type, nA, nD, nCc, nAv, nround, nsim = 1e+5, error=0){
 #simul("A", 5, 5, 5, 5, 5)
 #simul("D", 5, 5, 5, 5, 5)
 #simul("Cc", 5, 5, 5, 5, 5)
+
+stat.simul <- function(type, nA, nD, nCc, nAv, nround, nsim = 1e+5, error=0){
+  mat.0 <- Pm(type, nA, nD, nCc, nAv, error)
+  mat <- stationary(mat.0)
+  M <- ncol(mat); score1 <- score[1,floor(as.numeric(colnames(mat)[-1]))]
+  point <- unlist(c(0, score1)); 
+  x <- rep(0,nsim);
+  for (j in 1:nsim) {
+    temp <- sample(1:M, nround, replace=T, prob=mat)
+    x[j] <- sum(point[temp])
+  } 
+  return(list(table(x)/length(x), mean(x)))
+}
+
+#stat.simul("Av", 5, 5, 5, 5, 5)
+#stat.simul("A", 5, 5, 5, 5, 5)
+#stat.simul("D", 5, 5, 5, 5, 5)
+#stat.simul("Cc", 5, 5, 5, 5, 5)
